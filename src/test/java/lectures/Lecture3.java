43eea76d7ebe08b5368891b34fe6d75947bfbfdb
premvinodh <premvinodh.pc@gmail.com>
2026-06-03 15:53:14 +0530

    Lecture3 - Min, Max & Comparators - Finding min and max

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,23 +1,47 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
 import java.util.Comparator;
 import java.util.List;
-import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture3 {
 
-  @Test
-  public void min() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    /**
+     * find the max number using stream.min
+     *
+     * @throws Exception
+     */
+    @Test
+    public void min() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
-  }
+        Integer min = numbers.stream()
+                //.min((number1, number2) -> number1 > number2 ? 1 : -1) // Using explicit Comparator comparison
+                .min(Comparator.naturalOrder())
+                .get();
 
-  @Test
-  public void max() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+        assertThat(min).isEqualTo(1);
+        System.out.println(min);
+    }
 
-  }
+    /**
+     * find the max number using stream.max
+     *
+     * @throws Exception
+     */
+    @Test
+    public void max() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+
+        Integer max = numbers.stream()
+                .max(Comparator.naturalOrder())
+                .get();
+
+        assertThat(max).isEqualTo(100);
+        System.out.println(max);
+    }
 }
