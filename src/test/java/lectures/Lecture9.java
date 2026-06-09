45b258d726e7c94510b3a0a172fb623323bc05b8
premvinodh <premvinodh.pc@gmail.com>
2026-06-10 00:49:19 +0530

    Lecture9 - Reduce - sum all elements in the array using reduce

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,18 +1,27 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
 import org.junit.Test;
 
 public class Lecture9 {
 
-  @Test
-  public void reduce() throws Exception {
-    Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    /**
+     * Sum all the elements in the array using reduce
+     *
+     * @throws Exception
+     */
+    @Test
+    public void reduce() throws Exception {
+        Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
 
-  }
+        int sum = Arrays.stream(integers)
+            .reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
 
-
+        int sum2 = Arrays.stream(integers)
+            .reduce(0, Integer::sum);
+        System.out.println(sum2);
+    }
 }
 
