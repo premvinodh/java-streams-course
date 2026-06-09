12f7c66cbfbdbe31737b05d9af48ec07fb77332e
premvinodh <premvinodh.pc@gmail.com>
2026-06-09 22:16:24 +0530

    Lecture8 - Grouping data with streams like sql

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.assertj.core.util.Lists;
@@ -14,27 +13,46 @@ import org.junit.Test;
 
 public class Lecture8 {
 
-  @Test
-  public void simpleGrouping() throws Exception {
+    /**
+     * Group all the cars by 'make' This is similar to grouping in sql.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void simpleGrouping() throws Exception {
+        Map<String, List<Car>> grouping = MockData.getCars()
+            .stream()
+            .collect(Collectors.groupingBy(Car::getMake));
 
-  }
+        grouping.forEach((make, cars) -> {
+            System.out.println(make);
+            cars.forEach(System.out::println);
+        });
+    }
 
-  @Test
-  public void groupingAndCounting() throws Exception {
-    ArrayList<String> names = Lists
-        .newArrayList(
-            "John",
-            "John",
-            "Mariam",
-            "Alex",
-            "Mohammado",
-            "Mohammado",
-            "Vincent",
-            "Alex",
-            "Alex"
-        );
+    /**
+     * Count the no. of times each name is repeated in the list
+     *
+     * @throws Exception
+     */
+    @Test
+    public void groupingAndCounting() throws Exception {
+        ArrayList<String> names = Lists
+            .newArrayList(
+                "John",
+                "John",
+                "Mariam",
+                "Alex",
+                "Mohammado",
+                "Mohammado",
+                "Vincent",
+                "Alex",
+                "Alex"
+            );
 
+        Map<String, Long> counting = names.stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-  }
-
+        counting.forEach((name, count) -> System.out.println(name + " > " + count));
+    }
 }
