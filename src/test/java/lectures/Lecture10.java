3da234eb9b842f929a01b7768deeb97c00fe6bbd
premvinodh <premvinodh.pc@gmail.com>
2026-06-10 00:55:49 +0530

    Lecture10 - FlatMap - collapse 2D list of names to 1D list of names

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -10,28 +10,48 @@ import org.junit.Test;
 
 public class Lecture10 {
 
-  private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
-      Lists.newArrayList("Mariam", "Alex", "Ismail"),
-      Lists.newArrayList("John", "Alesha", "Andre"),
-      Lists.newArrayList("Susy", "Ali")
-  );
+    private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
+        Lists.newArrayList("Mariam", "Alex", "Ismail"),
+        Lists.newArrayList("John", "Alesha", "Andre"),
+        Lists.newArrayList("Susy", "Ali")
+    );
 
-  @Before
-  public void setUp() {
-    System.out.println(arrayListOfNames);
-  }
+    @Before
+    public void setUp() {
+        System.out.println(arrayListOfNames);
+    }
 
-  @Test
-  public void withoutFlatMap() throws Exception {
-//    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+    @Test
+    public void withoutFlatMap() throws Exception {
+        //    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> names = Lists.newArrayList();
 
-  }
+        for (List<String> listOfNames : arrayListOfNames) {
+            for (String name : listOfNames) {
+                names.add(name);
+            }
+        }
 
-  @Test
-  public void withFlatMap() throws Exception {
+        System.out.println(names);
+    }
+
+    /**
+     * <pre>
+     * Collapse list of list of names to list of names
+     * (or)
+     * Collapse 2D list of names (list of list of names) to 1D list of names
+     * </pre>
+     *
+     * @throws Exception
+     */
+    @Test
+    public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
 
-  }
+        List<String> names = arrayListOfNames.stream()
+            .flatMap(List::stream)
+            .collect(Collectors.toList());
 
+        System.out.println(names);
+    }
 }
-
